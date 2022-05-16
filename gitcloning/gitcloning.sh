a7c136b292c0e379510803ccaf9e90ea27095699
Arsh Sahzad <admin@arsh.dev>
2022-05-16 15:12:53 +0530

    gitcloning

--- gitcloning/gitcloning.sh
+++ gitcloning/gitcloning.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-#Get Repo Count
-read -p "How Many Repo You Want To Clone: " repo_count
-
-while [ $repo_count -gt 0 ]
-do
-#Type Repo Name
-read -p "Enter Repo Name: " repo_name
+#Enter Repository Name
+read -p "Enter Repository Name: " repo_name
 mkdir -p ./$repo_name && cd ./$repo_name
+echo "Created Directory for $repo_name"
 
 #Get Repo Branch Count
-read -p "How Many Repo Branch You Want To Clone: " branch_count
+read -p "No. of Branch to Clone for $repo_name: " branch_count
+while [ $branch_count -gt 0 ]
+do
 
-#Type  Branch Name
-read -p "Enter Branch Name: " branch_name
-repo_count=$(( $repo_count - 1 ))
+#Enter Branch Name
+read -p "Enter Branch Name of $repo_name: " branch_name
 mkdir -p ./$branch_name && cd ./$branch_name
 
 #Cloning Repo of Particular Branch
 git clone git@github.com:arshsahzad/$repo_name.git -b $branch_name
-cd ../
+cd ..
+
+#While Run Branch is = 0
 branch_count=$(( $branch_count - 1 ))
 done

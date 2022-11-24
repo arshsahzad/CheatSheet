86f3853a43a5f4475ddba0bd9a1c0dc51109becc
Arsh Sahzad <admin@arsh.dev>
2022-11-24 12:44:13 +0530

    backuptar

--- scriptbox/backuptar/backuptar.sh
+++ scriptbox/backuptar/backuptar.sh
@@ -2,26 +2,39 @@
 
 # Settings / Change This To Your Config
 volumeDir="/opt/gitlab/"
-backupDir="/opt/bakops/gitlab/"
-bucketDir="s3://oodles-infra-backups/gitlab-oodleslab-com/gitlab-fullbackup/"
+backupDir="/opt/backup/gitlab/"
+bucketDir="s3://oodles-infra-backups/gitlab-backups/"
 dateFormat=$(date "+%d-%m-%Y-%H%M%S")
 fileName=$"$dateFormat-GitLab.tar.gz"
 keepDays=3
 
+# Stops Execution If Error
 set -e
 
+# Printout Date & Time
 echo "$dateFormat"
 
-# Create Tar in Backup Directory
+# Create Backup Directory If Not Exist
+if [[ -d $backupDir ]]
+then
+echo "$backupDir Already Exist"
+else
+echo "Creating $backupDir Directory"
+mkdir -p $backupDir
+fi
+
+# Create Compressed Tar In Backup Directory
 echo "Generating GitLab Tar For Backup"
 tar -cvzf $backupDir$fileName -P $volumeDir
 
-# Upload Tar Backup to Amazon S3
-aws s3 cp $backupDir$fileName $bucketDir
+# Upload Compressed Tar To Amazon S3
+/usr/bin/aws s3 cp $backupDir$fileName $bucketDir
 
-# Delete Old Tar Backup
+# Change Directory To Backup
 if [ ! -z ${backupDir} ]; then
 cd ${backupDir}
-echo "Deleting Backups Older Than $keepDays Days"
+
+# Delete Backup Older Than 3 Days
+echo "Deleting Backup Older Than $keepDays Days"
 find . -name "*.tar.gz" -mtime +$keepDays -exec rm -rf {} \;
 fi
